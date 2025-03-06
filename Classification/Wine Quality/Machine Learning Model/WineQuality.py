4bc2ec84f3dbfeb2f2841ffd7566d08456777187
ekfas18 <skylineakias@gmail.com>
2025-03-06 14:55:38 +0200

    Completes the .ipynb and .py variations of Wine Quality case problem with simple machine learning models.

---
+++ Classification/Wine Quality/Machine Learning Model/WineQuality.py
@@ -0,0 +1,96 @@
+## Importing Libraries.
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+
+## Importing Dataset.
+dataset = pd.read_csv("winequality-white.csv", delimiter = ';')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+## Split data to Training and Testing datasets.
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+## Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+## Define the Machine Learning models.
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+## Hyperparameter Tuning function for RandomForectClassifier.
+def rfc():
+    from sklearn.model_selection import GridSearchCV
+    param_grid = {
+        'n_estimators': [50, 100, 200, 300],
+        'max_depth': [None, 10, 20, 30, 40],
+        'min_samples_split': [2, 5, 10, 20]
+    }
+    grid_search = GridSearchCV(estimator = RandomForestClassifier(), param_grid = param_grid, cv = 3, n_jobs = -1)
+    grid_search.fit(X_train, y_train)
+    best_rf_model = grid_search.best_estimator_
+    y_pred_rf = best_rf_model.predict(X_test)
+    
+    return y_pred_rf
+
+# Defines models and passes some basic parameters for each model.    
+models = {
+    "Logistic Regression": LogisticRegression(),
+    "K-Neighbours Classifier": KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2, n_jobs = -1),
+    "Support Vector Classifier": SVC(C = 1, gamma = 0.1),
+    "Gaussian Naive Bayes": GaussianNB(),
+    "Decision Tree Classifier": DecisionTreeClassifier(criterion = 'entropy'),
+    "Random Forest Classifier": rfc()
+}
+
+## Define metrics to measure the efficieny of each model.
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+# Initializes an empty list to store results.
+results = []
+for name, model in models.items():
+    if name != "Random Forest Classifier":
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+    else:
+         y_pred = model
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average = 'weighted')
+    precision = precision_score(y_test, y_pred, average = 'weighted')
+    recall = recall_score(y_test, y_pred, average = 'weighted')
+    
+    results.append({
+        "model": name,
+        "accuracy": round(accuracy, 4),
+        "f1-Score": round(f1, 4),
+        "precision": round(precision, 4),
+        "recall": round(recall, 4),
+    })
+    
+df_results = pd.DataFrame(results)
+
+# Initialize best_model dictionary
+best_model = {
+    'model': '',
+    'accuracy': 0.0
+}
+
+for index, row in df_results.iterrows():
+    accuracy = row['accuracy']
+    model = row['model']
+    
+    if best_model['accuracy'] < accuracy:
+        best_model['model'] = model
+        best_model['accuracy'] = accuracy
+        
+print(f"Best model is {best_model['model']} with accuracy {best_model['accuracy'] * 100}%")
---
+++ Classification/Wine Quality/readme.txt
@@ -0,0 +1,46 @@
+								======== UCI MACHINE LEARNING REPOSITORY ========
+
+
+Two datasets are included, related to red and white vinho verde wine samples, from the north of Portugal. The goal is to model wine quality based on physicochemical tests (see [Cortez et al., 2009], http://www3.dsi.uminho.pt/pcortez/wine/).
+
+Dataset Characteristics: Multivariate
+Subject Area: Business
+Associated Tasks: Classification, Regression
+Feature Type: Real
+
+=======================================================================================
+* link: https://archive.ics.uci.edu/dataset/186/wine+quality
+* Instances: 4898
+* Features: 11
+* Has Missing Values? : NO
+=======================================================================================
+Additional Information
+
+For more information, read [Cortez et al., 2009].
+Input variables (based on physicochemical tests):
+
+   1 - fixed acidity
+
+   2 - volatile acidity
+
+   3 - citric acid
+
+   4 - residual sugar
+
+   5 - chlorides
+
+   6 - free sulfur dioxide
+
+   7 - total sulfur dioxide
+
+   8 - density
+
+   9 - pH
+
+   10 - sulphates
+
+   11 - alcohol
+
+Output variable (based on sensory data): 
+
+   12 - quality (score between 0 and 10)
