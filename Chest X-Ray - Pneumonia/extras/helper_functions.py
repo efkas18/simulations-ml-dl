547e42dddddd606f8ef448076f003a4385a36271
efkas18 <efkarpidisgm@gmail.com>
2025-09-12 15:33:28 +0300

    Improves simulations by adding fine tune model.

--- Chest X-Ray - Pneumonia/extras/helper_functions.py
+++ Chest X-Ray - Pneumonia/extras/helper_functions.py
@@ -141,3 +141,48 @@ def custom_confusion_matrix(y_true, y_pred, title = "Confusion Matrix"):
                 cmap = "crest",
                 linewidth = 1);
     ax.set_title(title);
+
+    # Compares history of original model and after fine-tuning.
+def compare_histories(original_history, new_history, initial_epochs = 5):
+    """
+    Compares two TensorFlow model History objects.
+
+    Args:
+      original_history: History object from original model (before new_history)
+      new_history: History object from continued model training (after original_history)
+      initial_epochs: Number of epochs in original_history (new_history plot starts from here)
+    """
+
+    # Get original history measurements
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
+
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
+    # Combine original history with new history
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
+
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
+
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs, initial_epochs],
+             plt.ylim(), label='Start Fine Tuning')  # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs, initial_epochs],
+             plt.ylim(), label='Start Fine Tuning')  # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
